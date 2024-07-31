a75a7f31e8942c343e45ee67a3dbe29d2dfe5012
aidansunbury <aidansunbury@gmail.com>
2024-07-31 13:45:46 -0700

    test change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "61D",
-  tagline: "Dinosaurs are cool",
+  tagline: "Typescript is cool",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
